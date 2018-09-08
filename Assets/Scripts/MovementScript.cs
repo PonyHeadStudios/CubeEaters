21eae3c7c3689c90ecac87f849b7ca7c4c6591e0
PonyHeadStudios <PonyHeadStudios@users.noreply.github.com>
2018-09-08 00:15:41 -0300

    0.0.1a

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MovementScript : MonoBehaviour {
+
+    public TileManagerScript tileManager;
+
+	void Update ()
+    {        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 newPos;
+            if (tileManager.CheckValidCell(worldPos, out newPos))
+            {
+                transform.position = newPos;
+            }
+                  
+        }		
+	}    
+}
---
+++ Assets/Scripts/TileManagerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileManagerScript : MonoBehaviour {
+
+    private Tilemap groundTilemap;
+
+    private void Start()
+    {
+        groundTilemap = GetComponent<Tilemap>();
+    }
+
+    public bool CheckValidCell (Vector3 v3, out Vector3 newPos) 
+    {
+        Vector3Int worldPosInt = groundTilemap.WorldToCell(v3);
+        Tile tileClicked = groundTilemap.GetTile<Tile>(worldPosInt);
+        newPos = groundTilemap.GetCellCenterWorld(worldPosInt);        
+        if (tileClicked != null)
+        {
+            if (tileClicked.name == "Square") //Cuando haya mas cuadros, crear metodo de verificacion
+            {
+                
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }            
+    }
+}
